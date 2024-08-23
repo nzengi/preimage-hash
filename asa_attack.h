f2f8923add5a4309b6819914a331215f53c43698
nzengi <zenginureddin1@gmail.com>
2024-08-24 01:59:27 +0300

    Create asa_attack.h

---
+++ asa_attack.h
@@ -0,0 +1,9 @@
+#ifndef ASA_ATTACK_H
+#define ASA_ATTACK_H
+
+#include <string>
+
+// ASA saldırısı için hash fonksiyonlarını değiştirir
+std::string manipulate_hash_function(const std::string& data);
+
+#endif // ASA_ATTACK_H
