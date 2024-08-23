392a3f845113ffb74a452ef3820a41c85aa41d3f
nzengi <zenginureddin1@gmail.com>
2024-08-24 02:01:19 +0300

    Create parallel_processing.h

---
+++ parallel_processing.h
@@ -0,0 +1,9 @@
+#ifndef PARALLEL_PROCESSING_H
+#define PARALLEL_PROCESSING_H
+
+#include <string>
+
+// Paralel işlem ile preimage saldırısı gerçekleştir
+std::string parallel_preimage_attack(const std::string& target_hash);
+
+#endif // PARALLEL_PROCESSING_H
