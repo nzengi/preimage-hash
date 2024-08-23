7cce8497ef8c9280b0781da724bdf32f660e4f88
nzengi <zenginureddin1@gmail.com>
2024-08-24 02:00:54 +0300

    Create hash_functions.cpp

---
+++ hash_functions.cpp
@@ -0,0 +1,33 @@
+#include "hash_functions.h"
+#include <openssl/sha.h>
+#include <openssl/ripemd.h>
+#include <iomanip>
+#include <sstream>
+
+std::string sha256(const std::string& data) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, data.c_str(), data.size());
+    SHA256_Final(hash, &sha256);
+    
+    std::stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
+std::string ripemd160(const std::string& data) {
+    unsigned char hash[RIPEMD160_DIGEST_LENGTH];
+    RIPEMD160_CTX ripemd160;
+    RIPEMD160_Init(&ripemd160);
+    RIPEMD160_Update(&ripemd160, data.c_str(), data.size());
+    RIPEMD160_Final(hash, &ripemd160);
+    
+    std::stringstream ss;
+    for(int i = 0; i < RIPEMD160_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
